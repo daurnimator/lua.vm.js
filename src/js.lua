8dbe78813cbf1d942c04ec49d1a8e6802162cfc5
Daurnimator <quae@daurnimator.com>
2014-08-12 16:02:14 -0400

    src/js.lua: Add __ipairs and __pairs

--- src/js.lua
+++ src/js.lua
@@ -1,14 +1,34 @@
 -- Make window object a global
 window = js.global;
 
--- Iterates from 0 to collection.length-1
-local function js_inext(collection, i)
-  i = i + 1
-  if i >= collection.length then return nil end
-  return i, collection[i]
-end
-function js.ipairs(collection)
-  return js_inext, collection, -1
+do -- Create js.ipairs and js.pairs functions. attach as __pairs and __ipairs on JS userdata objects.
+	local _PROXY_MT = debug.getregistry()._PROXY_MT
+
+	-- Iterates from 0 to collection.length-1
+	local function js_inext(collection, i)
+		i = i + 1
+		if i >= collection.length then return nil end
+		return i, collection[i]
+	end
+	function js.ipairs(collection)
+		return js_inext, collection, -1
+	end
+	_PROXY_MT.__ipairs = js.ipairs
+
+	local function js_next(collection, last)
+		if i >= collection.length then return nil end
+		return i, collection[i]
+	end
+	function js.pairs(ob)
+		local keys = js.global.Object:getOwnPropertyNames(ob) -- Should this be Object.keys?
+		local i = 0
+		return function(ob, last)
+			local k = keys[i]
+			i = i + 1;
+			return k, ob[k]
+		end, ob, nil
+	end
+	_PROXY_MT.__pairs = js.pairs
 end
 
 local function load_lua_over_http(url)
