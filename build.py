622d325f32af4f2dddd89ce128c519a8d64f4819
Daurnimator <quae@daurnimator.com>
2014-08-12 15:44:14 -0400

    build.py: add shebang, clean up

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
-import os
+#!/usr/bin/env python3
+
 import json
 
 f = open('dist/lua.vm.js', 'w')
@@ -6,4 +7,3 @@ f.write(open('lua/src/liblua.js').read())
 js_lua = json.dumps(open('src/js.lua').read())
 f.write(open('src/lua.js').read().replace('{{{ JS_LUA }}}', js_lua))
 f.close()
-
